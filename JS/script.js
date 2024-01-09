b7acd84a5d9fce175901a9b7d0b054519cc08b6d
Abhishek Kumar <avykr07@gmail.com>
2024-01-09 20:46:31 +0530

    local storage holds favourite movies now

--- JS/script.js
+++ JS/script.js
@@ -7,22 +7,35 @@ let movies = []; // array which contains movie list
 
 const movieListELEM = document.getElementById("movies-list");
 
+// we will maintain an array in local storage
+// [{}, {}, {}, {}, ]
+// the array will contain objects, i.e. the movie objects that we mark as favourite 
 function getFavMoviesFromLocalStorage(){
+    // this function grabs the fav movies array from local storage
+    // localStorage.getItem("favouriteMovie") will give us data in JSON string format, which is we need to parse It to make object
     const favMovies = JSON.parse(localStorage.getItem("favouriteMovie"));
-    return favMovies === null ? [] : favMovies;
+    return favMovies === null ? [] : favMovies; //that is when there is no movies set as favourite
+    // so when we try to grab our favourite movies array for the first time we will get null which is undesirable
+    // instead we return an empty array
 }
 
-function addMovieInfoINLocalStorage(mInfo){
-    if(mInfo.length === 0) {
+function addFavMovieInfoINLocalStorage(mInfo){
 
-    }
     const localStorageMovies = getFavMoviesFromLocalStorage();
-    localStorage.setItem("favouriteMovie",JSON.stringify([...localStorageMovies, ...mInfo]));
+    for(let i = 0; i < localStorageMovies.length; i++){
+        if(localStorageMovies[i].title === mInfo.title){
+            return;
+        }
+    }
+    
+    localStorage.setItem("favouriteMovie",JSON.stringify([...localStorageMovies, mInfo]));
 }
 
 function removeFavMovieFromLocalStorage (mInfo){
-    const localStorageMovies = getFavMoviesFromLocalStorage();
+    const localStorageMovies = getFavMoviesFromLocalStorage(); // localStorageMovies is an array
     const filteredMovies = localStorageMovies.filter((eachMovie) => eachMovie.title != mInfo.title);
+    // loop on movies, filter out movie which title wont match the movie-title we are removing
+    // all other movie excpet the movie we are removing will move into filteredMovies
     localStorage.setItem("favouriteMovie", JSON.stringify(filteredMovies));
 
 }
@@ -39,14 +52,25 @@ function renderMovies(movies = []) {
     
     const favMovies = getFavMoviesFromLocalStorage();
 
-    favMovies.reduce((acc, curr) => {
-        console.log(acc, curr);
-        return curr;
-    }, `{}`)
+
+    // let us create a movie map by using reduce
+    // key of our map will be title
+    // value will be true or false indicating whether it has been favorited or not
+    // title : {}
+
+    const favMoviesMAPPING = favMovies.reduce((acc, curr) =>{
+        acc[curr.title] = true; //{"Your Name" : true, "The Vow" : true}
+        return acc;
+    }, {})
 
     movies.forEach((eachMovie) => {
-
-        const alreadyFavourited = favMovies.find((e) => e.title === eachMovie.title);
+        // grab the fav movies from local storage inside the favMovies
+        // search if eachMovie is present inside the favMovies i.e. Has it been favourited?
+        // if it is present add the class fa-solid so that on the webpage the heart icon is already filled
+        // this is useful if user marked some movies as favourite and reloaded page / closed tab
+        
+        // const alreadyFavourited = favMovies.find((e) => e.title === eachMovie.title); 
+        // not a good method because for each movie we are looping on the favMovies again and again
         const {poster_path, title, vote_average, vote_count} = eachMovie;
         let listItem = document.createElement("li");
         listItem.className = "card";
@@ -59,43 +83,46 @@ function renderMovies(movies = []) {
             vote_count,
             poster_path,
         };
-        console.log("MOPVIE INFOE", movieInfo);
+        // console.log("MOPVIE INFOE", movieInfo);
 
+        const ifFav = favMoviesMAPPING[title];
         let html = `
-            <img class="poster" src=${imageURL}
-                    alt= ${title}
-            />
+            <img class="poster" src=${imageURL} alt= ${title}/>
                 <p class="title">${title}</p>
                 <section class="vote-fav">
                     <section>
                         <p>Votes: ${vote_count}</p>
                         <p>Rating: ${vote_average}</p>
                     </section>
-                    <i minfo='${JSON.stringify(movieInfo)}' class="fa-regular fa-heart fa-2xl fav-icon"></i>
+                    <i mInfo='${JSON.stringify(movieInfo)}' class="fa-regular fa-heart fa-2xl fav-icon ${ifFav ? "fa-solid" : ""}"></i>
                 </section>
         `;
         
-                listItem.innerHTML = html;
-                movieListELEM.appendChild(listItem);
+        
+        listItem.innerHTML = html;
+        movieListELEM.appendChild(listItem);
 
         const favIconBTN = listItem.querySelector(".fav-icon");
         favIconBTN.addEventListener("click", (event)=>{
-            console.log(event.target);
-            console.log("MINFO", event.target.getAttribute("minfo"))
-            // let mInfo = JSON.parse(event.target.getAttribute("minfo"));
-            // mInfo = JSON.parse(mInfo);
-            // console.log(mInfo);
+            // console.log(event.target);
+            // console.log("MINFO", event.target.getAttribute("mInfo"))
+
+            let mInfo = JSON.parse(event.target.getAttribute("mInfo"));
+            // console.log("MINFOOO",mInfo);
 
             if(favIconBTN.classList.contains("fa-solid")){
                 // unmark it
                 // remove the class to unmark the movie visually
                 favIconBTN.classList.remove("fa-solid")
                 // remove this movie info from the local storage
-                // removeFavMovieFromLocalStorage(mInfo);
+                removeFavMovieFromLocalStorage(mInfo);
             } else {
                 // mark it
+                // add the class to unmark the movie visually
+
                 favIconBTN.classList.add("fa-solid")
-                // addMovieInfoINLocalStorage(mInfo);
+                // add the movie info into local storage
+                addFavMovieInfoINLocalStorage(mInfo);
             }
         })
 
@@ -107,11 +134,17 @@ async function fetchMovies(){
         const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${APIKEY}&language=en-US&page=1`);
         let data = await response.json();
         console.log(data);
-
+        // in the api endpoint we are fetching only top rated movies
+        // language of our data is ENG-US
+        // and we are only requesting page number 1 of our API
         movies = data.results; // the movies that we got from API call is being stored inside movies array
+        // the api provides a results property which is an array of object
+        // each object inside the results array contains the details of movies
+
         totaPages = data.total_pages; 
 
-        renderMovies(movies);
+        renderMovies(movies); //render each movies on the html page
+        //movielist contains the all the movie cards
     } catch(error){
         console.log(error);
     }
--- localStorage.js
+++ localStorage.js
@@ -1,2 +1,40 @@
 // browser memory
-// 
+// allows us to store some data inside the browser
+// caching, local storage
+/* 
+Local storage is a web storage feature provided by web browsers that allows websites to store data locally on a user's device. 
+***It is specific to a particular domain, meaning that data stored in local storage by a website is only accessible by that same website on the same domain.
+
+Local storage is not inherently tab-specific; instead, it is domain-specific. 
+    This means that if you open multiple tabs from the same website (with the same domain), 
+        they will share the same local storage. Each tab does not have its own isolated local storage.
+
+The data stored in local storage is also URL specific in the sense that it is tied to the domain (and, optionally, the protocol and port) of the website. 
+    If you navigate to a different domain, the local storage for the previous domain will not be accessible.
+
+To clarify, if you have multiple tabs open for the same website, they will share the same local storage.
+     However, if you open a new tab and navigate to a different website, 
+        the local storage of the previous website will not be accessible in the new tab.
+*/
+//  local-storage is a global OBJECT available to us inside window object only
+// write => window.localStorage in console to get the localStorage object
+
+// local storage provides us key-value pair BECZ it is an object
+// it has methods to store and send data
+
+// getItem is supposed to get us a particular given local storage data BY USING the key, bcz localStorage is an object
+
+
+// localStorage.getItem
+// localStorage.setItem
+// localStorage.removeItem
+// localStorage.clear
+// localStorage.key
+// localStorage.length
+
+localStorage.setItem("name", "avy");
+localStorage.getItem("name");
+console.log(localStorage.getItem("name"))
+
+// DO NOT STORE VALUES OF OBJECTS OR ARRAYS IN THE KEY-VALUE PAIR
+// USE JSON.stringify() TO CONVERT ARRAY OR OBJECT INTO JSON-STRING 
